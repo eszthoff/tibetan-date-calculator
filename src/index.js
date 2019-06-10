392ed093c63a19986feafa524b0cb9b86a94fbd7
Eszter Hoffmann <eszt.hoff@gmail.com>
2019-06-10 21:00:01 +0200

    fix export to match latest changes and remove old calendars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-export { default as getCalendarForYear } from './generators/get-calendar-for-year';
-export { default as getYearFromRabjung } from './generators/get-year-from-rabjung';
-export { default as getYearFromTibetan } from './generators/get-year-from-tibetan';
-export { default as getYearFromWestern } from './generators/get-year-from-western';
 export { default as getLosarForYear } from './generators/get-losar-for-year';
-export { default as getMonthFromTibetan } from './generators/get-month-from-tibetan';
 export { default as TibetanDate } from './classes/tibetan-date';
 export { default as TibetanMonth } from './classes/tibetan-month';
+export { default as TibetanYear } from './classes/tibetan-year';

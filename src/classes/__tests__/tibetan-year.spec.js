63bb81bebe656376831cc53c04ac5daacf77c2d8
Eszter Hoffmann <eszt.hoff@gmail.com>
2019-06-10 20:18:25 +0200

    add TibetanYear class

---
+++ src/classes/__tests__/tibetan-year.spec.js
@@ -0,0 +1,37 @@
+import MockDate from 'mockdate';
+import TibetanYear from '../tibetan-year';
+
+describe('TibetanYear', () => {
+  beforeAll(() => {
+    MockDate.set('2019/06/07');
+  });
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  it('should create a correct class when invoked without arg', () => {
+    expect(new TibetanYear()).toMatchSnapshot();
+  });
+  it('should create a correct class when invoked with number arg', () => {
+    expect(new TibetanYear(2145)).toMatchSnapshot();
+  });
+  it('should create a correct class when invoked with number arg and western set to true', () => {
+    expect(new TibetanYear(2013, true)).toMatchSnapshot();
+  });
+  it('should create a correct class when invoked with object arg', () => {
+    expect(new TibetanYear({
+        rabjungCycle: 17, rabjungYear: 33
+    })).toMatchSnapshot();
+  });
+  it('should not calculate months when initially incoved', () => {
+    const year = new TibetanYear();
+    expect(year.months.length).toEqual(0);
+  });
+  it('should not calculate months when requested', () => {
+    const year = new TibetanYear();
+    const months = year.getMonths();
+
+    expect(year.months.length).not.toEqual(0);
+    expect(months.length).toEqual(13);
+  });
+});
